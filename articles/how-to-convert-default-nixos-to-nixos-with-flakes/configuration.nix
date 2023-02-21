7734a2e983109a00debad927a3ca013031168b38
drakerossman <drake.rossman@protonmail.com>
2023-02-21 14:56:13 +0300

    bump system.stateVersion

--- articles/how-to-convert-default-nixos-to-nixos-with-flakes/configuration.nix
+++ articles/how-to-convert-default-nixos-to-nixos-with-flakes/configuration.nix
@@ -124,5 +124,5 @@
     ];
   };
 
-  system.stateVersion = "22.05";
+  system.stateVersion = "22.11";
 }
--- articles/nixos-for-apt-yum-users/configuration.nix
+++ articles/nixos-for-apt-yum-users/configuration.nix
@@ -117,5 +117,5 @@
     ];
   };
 
-  system.stateVersion = "22.05";
+  system.stateVersion = "22.11";
 }
